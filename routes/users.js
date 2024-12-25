215cfc7c976c4bede0579d445649f10bdb11d3d9
ChristianRojasOliver <christian.rojas201@gmail.com>
2024-12-25 14:16:51 -0500

    delete method

--- routes/users.js
+++ routes/users.js
@@ -36,20 +36,25 @@ router.get("/", (req, res) => {
 
 // GET by specific ID request: Retrieve a single user with email ID
 router.get("/:email", (req, res) => {
+    // Extract the email parameter from the request URL
     const email = req.params.email;
+    // Filter the users array to find users whose email matches the extracted email parameter
     let filtered_users = users.filter((user) => user.email === email);
+    // Send the filtered_users array as the response to the client
     res.send(filtered_users);
 });
 
 
 // POST request: Create a new user
 router.post("/", (req, res) => {
+    // Push a new user object into the users array base on query parameters from the request
     users.push({
         "firstName": req.query.firstName,
         "lastName": req.query.lastName,
         "email": req.query.email,
         "DOB": req.query.DOB,
     })
+    // Send a succes message as the response, indicating the user has been added
     res.send("The user " + req.query.firstName + " has been added!")
 });
 
@@ -97,8 +102,12 @@ router.put("/:email", (req, res) => {
 
 // DELETE request: Delete a user by email ID
 router.delete("/:email", (req, res) => {
-    // Copy the code here
-    res.send("Yet to be implemented")//This line is to be replaced with actual return value
+    // Extract the email parameter from the request URL
+    const email = req.params.email;
+    // Filter the users array to exclude the user with the specified email
+    users = users.filter((user) => user.email != email);
+    // Send a succes message as the response, indicating the user has been deleted
+    res.send(`User with the email ${email} deleted`);
 });
 
 module.exports = router;
